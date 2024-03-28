cb901b60861658c5172c037d4d6e1a47bfb6b764
amrks7800 <amr.khaled_a646@fsh.kfs.edu.eg>
2024-03-28 09:44:26 +0200

    Fix: cors config origin

--- server.js
+++ server.js
@@ -16,7 +16,12 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
-app.use(cors())
+app.use(
+  cors({
+    optionsSuccessStatus: 200,
+    origin: false,
+  })
+)
 
 // Routes
 app.use("/user", userRouter)
